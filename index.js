d4f51e83d6d6e521fc432b17b7c6ee013bee39bb
Sujin <sujinbonnaud@gmail.com>
2023-11-24 18:56:33 -0600

    run it with json(js obj notation)

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
 app.get('/dog', function (req, res) {
-    res.send('wolf')
+  res.send({'sound' : 'dog dog'})
 })
 
 app.get('/cat', function (req, res) {
-    res.send('miya')
+  res.send('Hello World')
 })
 
 app.listen(3000)
