cb3721ebe11924a1dddbdfada51b617734b74e01
Sujin <sujinbonnaud@gmail.com>
2023-11-25 19:40:16 -0600

    use if else cases to response diff

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/user/:id', function (req, res) {
-  // as a parameter
-  // const q = req.params
-  // console.log(q)
+app.get('/sound/:name', function (req, res) {
+  const { name } = req.params
 
-  // as a query
-  const q = req.query
-  console.log(q)
-
-  res.json({'userid' : q.id})
-})
-
-app.get('/cat', function (req, res) {
-  res.json({'sound' : 'cat cat'})
+  if(name == "dog") {
+    res.json({'sound' : 'dog dog'})
+  } else if(name == "cat") {
+    res.json({'sound' : 'cat cat'})
+  } else if(name == "pig") {
+    res.json({'sound' : 'pig pig'})
+  } else {
+    res.json({'sound' : 'no no'})
+  }
 })
 
 app.listen(3000)
