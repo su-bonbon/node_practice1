954097458f99c5d9504cf33f2b68eff80708cfbc
Sujin <sujinbonnaud@gmail.com>
2023-11-25 19:22:41 -0600

    convert request as parameter

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/dog', function (req, res) {
-  res.send({'sound' : 'dog dog'})
+app.get('/user/:id', function (req, res) {
+  const q = req.params
+  console.log(q)
+
+
+
+  res.json({'sound' : 'dog dog'})
 })
 
 app.get('/cat', function (req, res) {
-  res.send('Hello World')
+  res.json({'sound' : 'cat cat'})
 })
 
 app.listen(3000)
