18bf49fee94b1f5e2159a83de0831aaa000745d0
Sujin <sujinbonnaud@gmail.com>
2023-11-27 21:51:42 -0600

    understanding basic express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+//routing
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
