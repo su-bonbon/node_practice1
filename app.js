4093df54374584b406eac8ba49b27288627824e6
Sujin <sujinbonnaud@gmail.com>
2023-11-27 21:59:04 -0600

    rendering using ejs

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const ejs = require('ejs');
 const app = express()
 const port = 3000
 
 
+app.set('view engine', 'ejs')
+app.set('views', './views')
 //routing
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.render('index')  // ./views/index.ejs
 })
 
 app.get('/test', (req, res) => {
